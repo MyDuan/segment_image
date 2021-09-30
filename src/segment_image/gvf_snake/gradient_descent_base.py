fe2dbe2b6b3d4dbd045e95d189a108d88937282d
MyDuan <dy1226130575@163.com>
2021-09-30 09:01:39 +0900

    add gvf method

---
+++ src/segment_image/gvf_snake/gradient_descent_base.py
@@ -0,0 +1,23 @@
+FLOAT_MAX = 3.40282e+38
+
+
+class GradientDecentBase:
+    def __init__(self, step_size):
+        self.step_size = step_size
+        self.last_energy_ = FLOAT_MAX
+
+    def run(self, max_iteration):
+        self.initialize()
+        self.last_energy_ = self.compute_energy()
+        print("base---")
+        print(self.last_energy_)
+        current_iter = 0
+        while not self.is_terminate(current_iter, max_iteration):
+            current_iter += 1
+            self.update()
+            new_energy = self.compute_energy()
+            if (new_energy < self.last_energy_):
+                self.last_energy_ = new_energy
+
+    def is_terminate(self, current_iter, max_iteration):
+        return current_iter >= max_iteration
---
+++ src/segment_image/gvf_snake/snake.py
@@ -0,0 +1,16 @@
+class ParamSnake:
+    def __init__(self, alpha, beta, step_size):
+        self.alpha = alpha
+        self.beta = beta
+        self.step_size = step_size
+
+
+class Contour:
+    def __init__(self, max_x, max_y, radius, center, num_points):
+        pi = 3.1415926
+        theta = 2.0 * pi / num_points
+
+
+class Snake:
+    def __init__(self, step_size):
+        pass
